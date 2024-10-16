dfbae3f6f5a6f65384fd84c325ae2d354ba217d0
Rosalie-A <61372066+Rosalie-A@users.noreply.github.com>
2024-10-16 13:43:37 -0400

    might not need this

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup  # type: ignore
-# https://github.com/pypa/setuptools/issues/2345
-
-if __name__ == "__main__":
-    setup(packages=[
-        "harp",
-		"zsprite"
-    ], use_scm_version=True)
